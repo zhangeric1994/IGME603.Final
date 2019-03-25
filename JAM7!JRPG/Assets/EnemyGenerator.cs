54bd6bdec3c4f3ab605f7a9c346229ea811239d4
Haoyu Guo (RIT Student) <hg7364@ad.rit.edu>
2019-03-25 16:05:47 -0400

    update

--- JAM7!JRPG/Assets/EnemyGenerator.cs
+++ JAM7!JRPG/Assets/EnemyGenerator.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class EnemyGenerator : MonoBehaviour
 {
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private int num;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    public void Active() {
+        for (int i = 0; i < num; i++) {
+            Instantiate(enemy, transform.position, Quaternion.identity);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
