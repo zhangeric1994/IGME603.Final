79917b91ac134cd1aee18640fbbd4d9b0d76c8f8
Runxuan Shu <shurunxuan@hotmail.com>
2019-03-26 08:50:32 -0400

    Change the camera to track only one player

--- JAM7!JRPG/Assets/Script/Utility/OverworldCamera.cs
+++ JAM7!JRPG/Assets/Script/Utility/OverworldCamera.cs
@@ -1,60 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class OverworldCamera : MonoBehaviour
 {
-	[SerializeField]
-	GameObject[] players;
-	[SerializeField]
-	float camMin;
-	[SerializeField]
-	float camMax;
-	Camera cam;
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    float camMin;
+    [SerializeField]
+    float camMax;
+    Camera cam;
 
     // Start is called before the first frame update
     void Start()
     {
-		cam = GetComponent<Camera>();
+        cam = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-		Vector3 furthest = cam.WorldToViewportPoint(players[0].transform.position);
-		Vector3 center = Vector3.zero;
+        Vector3 furthest = cam.WorldToViewportPoint(player.transform.position);
+        Vector3 center = Vector3.zero;
 
-		for(int i = 1; i < players.Length; i++)
-		{
-			Vector3 toView = cam.WorldToViewportPoint(players[i].transform.position);
 
-			if(Vector3.Distance(toView, new Vector3(0.5f, 0.5f, 0.0f)) >
-			   Vector3.Distance(furthest, new Vector3(0.5f, 0.5f, 0.0f)))
-			{
-				furthest = toView;
-			}
+        Vector3 toView = cam.WorldToViewportPoint(player.transform.position);
 
-			center += players[i].transform.position;
-		}
+        if (Vector3.Distance(toView, new Vector3(0.5f, 0.5f, 0.0f)) >
+           Vector3.Distance(furthest, new Vector3(0.5f, 0.5f, 0.0f)))
+        {
+            furthest = toView;
+        }
 
-		center /= players.Length;
-		center.z = -10.0f;
-		print(center);
+        center = player.transform.position;
 
-		transform.position = center;
+        center.z = -10.0f;
+        print(center);
 
-		float absx = Mathf.Abs(furthest.x);
-		float absy = Mathf.Abs(furthest.y);
+        transform.position = center;
 
-		if((absx > 0.99f || absx < 0.01f || absy > 0.99f || absy < 0.01f) && cam.orthographicSize < camMax)
-		{
-			cam.orthographicSize += 0.01f;
-		}
+        float absx = Mathf.Abs(furthest.x);
+        float absy = Mathf.Abs(furthest.y);
 
-		else if(((absx < 0.6f && absx > 0.4f) || (absy < 0.6f && absy > 0.4f))
-				&& cam.orthographicSize > camMin)
-		{
-			cam.orthographicSize -= 0.01f;
-		}
+        if ((absx > 0.99f || absx < 0.01f || absy > 0.99f || absy < 0.01f) && cam.orthographicSize < camMax)
+        {
+            cam.orthographicSize += 0.01f;
+        }
+
+        else if (((absx < 0.6f && absx > 0.4f) || (absy < 0.6f && absy > 0.4f))
+                && cam.orthographicSize > camMin)
+        {
+            cam.orthographicSize -= 0.01f;
+        }
     }
 }
